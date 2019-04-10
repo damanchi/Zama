a836ecb21f7be4b382204c9e690e566149cf2cd3
chidera Jonathan <chideraemerenini@gmail.com>
2019-04-10 09:35:18 +0100

    Initial Commit

---
+++ Zama/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Zama
+//
+//  Created by mac on 4/10/19.
+//  Copyright © 2019 appicreative. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
